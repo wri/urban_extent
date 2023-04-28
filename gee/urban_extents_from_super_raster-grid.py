322916ab82f555e4e70303d5a833d78d4867a329
brookie guzder-williams <brookieguzder-williams@brookies-mbp.lan>
2023-04-28 15:08:26 -0700

    add ue from grid

--- gee/urban_extents_from_super_raster-grid.py
+++ gee/urban_extents_from_super_raster-grid.py
@@ -4,15 +4,41 @@ import data
 import math
 import re
 from pprint import pprint
+
+"""
+BU=projects/wri-datalab/cities/urban_land_use/data/builtup_density_GHSL_WSF1519_WC21
+
+GRID=projects/wri-datalab/cities/urban_land_use/data/super_extents/dissolved_utm_grid-s5e4
+
+GRID_SIZE=5e4
+GRID_PROJ='epsg:4326'
+
+geom=city.geometry()
+
+
+
+var cell_vec=function(cell){
+  cell=ee.Feature(cell)
+  var vecs=city.selfMask().reduceToVectors({
+    geometry: cell.geometry(),
+    scale:10,
+    maxPixels: 1e11
+  })
+  return vecs
+}
+
+var vecs=ee.FeatureCollection(grid.map(cell_vec)).flatten()
+"""
+
 ee.Initialize()
 #
 # CONFIG
 #
-REGION_INDEX=2
+REGION_INDEX=5
 
 VECTOR_SCALE=None
 INPUT_VECTOR_SCALE=VECTOR_SCALE
-LIMIT=None
+LIMIT=3
 DRY_RUN=False
 RASTER_BUFFER=False
 VECTOR_BUFFER=None
@@ -40,6 +66,9 @@ SPLIT_INDEX=1
 #
 # CONSTANTS
 #
+GRID_SIZE=5e4
+GRID_PROJ='epsg:4326'
+
 REGIONS=[
   'East Asia and the Pacific (EAP)',    # 0
   'Europe and Japan (E&J)',   # 1
@@ -142,21 +171,25 @@ def add_coord_length(feat):
     })
 
 
-def urban_extent(im):
-  im=ee.Image(im)
-  bu=im.select('builtup')
-  bu_class=im.select('builtup_class')
-  pa=ee.Image.pixelArea()
-  bu_pixels=bu_class.gt(0).multiply(bu).selfMask()
-  if RASTER_BUFFER:
-    bu_pixels=bu_pixels.distance(
-      kernel=RASTER_BUFFER_KERNEL,
-      skipMasked=False).gte(0)
-  pa=bu_pixels.selfMask().addBands([
-      pa.multiply(bu_class.eq(0)),
-      pa.multiply(bu_class.eq(1)),
-      pa.multiply(bu_class.eq(2))
-  ]).rename(['label']+AREA_PROPS)
+def urban_grid(bu_class):
+  geom=bu_class.geometry()
+  grid=geom.convexHull().coveringGrid(GRID_PROJ,GRID_SIZE)
+  def cut_grid(cell):
+    cell=ee.Feature(cell)
+    g=geom.intersection(cell.geometry(),1)
+    return ee.Feature(g).copyProperties(cell)
+  grid=grid.map(cut_grid)
+  grid=bu_class.reduceRegions(
+    collection=grid, 
+    reducer=ee.Reducer.max(), 
+    scale=5, 
+    crs=GRID_PROJ)
+  return grid.filter(ee.Filter.eq('max',2))
+
+
+def get_cell_feature(cell,pa): 
+  cell=ee.Feature(cell)
+  pa=ee.Image(pa)
   feats=pa.reduceToVectors(
     reducer=ee.Reducer.sum(),
     crs=GHSL_CRS, 
@@ -185,7 +218,33 @@ def urban_extent(im):
     feat=feat.buffer(VECTOR_BUFFER,MAX_ERR)
   if ENSURE_FEATS:
     feat=feat.set('nb_input_features',feats.size())
-  return feat.copyProperties(im)
+  return feat
+
+
+def urban_extent(im):
+  im=ee.Image(im)
+  bu=im.select('builtup')
+  bu_class=im.select('builtup_class')
+  grid=urban_grid(bu_class)
+  bu_pixels=bu_class.gt(0).multiply(bu).selfMask()
+
+  if RASTER_BUFFER:
+    bu_pixels=bu_pixels.distance(
+      kernel=RASTER_BUFFER_KERNEL,
+      skipMasked=False).gte(0)
+
+  pa=ee.Image.pixelArea()
+  pa=bu_pixels.selfMask().addBands([
+      pa.multiply(bu_class.eq(0)),
+      pa.multiply(bu_class.eq(1)),
+      pa.multiply(bu_class.eq(2))
+  ]).rename(['label']+AREA_PROPS)
+
+  def _cell_vec(cell):
+    return get_cell_feature(cell,pa)
+  vecs=grid.map(_cell_vec)
+
+  return ee.Feature(vecs.geometry().dissolve(1)).copyProperties(im)
 
 
 
@@ -209,8 +268,10 @@ if TEST_CITY:
   SUPER_IC=SUPER_IC.filter(ee.Filter.eq('City__Name',TEST_CITY))
 else:
   if LIMIT:
-    SUPER_IC=SUPER_IC.limit(LIMIT,'Pop_2010')
-    name=f'{name}-lim{LIMIT}'
+    # SUPER_IC=SUPER_IC.limit(LIMIT,'Pop_2010')
+    # name=f'{name}-lim{LIMIT}'
+    SUPER_IC=SUPER_IC.limit(LIMIT,'Pop_2010',False)
+    name=f'{name}-lim{LIMIT}-big-reg{REGION_INDEX}'
   else:
     SUPER_IC=SUPER_IC.sort('Pop_2010')
 
@@ -227,7 +288,9 @@ if VECTOR_SCALE:
 if RASTER_BUFFER:
   name=f'{name}-rb{RASTER_BUFFER}-smask'
 description=re.sub('[\.\,\/]','--',name)
-asset_id=f'{ROOT}/urban_extents/{name}'
+# asset_id=f'{ROOT}/urban_extents/{name}'
+asset_id=f'projects/wri-datalab/cities/urban_land_use/dev/{name}'
+
 print('\n'*1)
 print(f'EXPORTING [{SUPER_IC.size().getInfo()}]:',asset_id)
 task=ee.batch.Export.table.toAsset(
